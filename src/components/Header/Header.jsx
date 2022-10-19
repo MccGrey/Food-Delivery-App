8efd2509486aa0a98729f3be8a19d512959f59de
MccGrey <chuxzzie01@yahoo.com>
2022-10-19 05:42:30 +0100

    add logo to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import { Container } from "reactstrap";
+import logo from "../../assets/images/res-logo.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <Container>
+        <div className="nav_wrapper">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+            <h5>Tasty Treat</h5>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
