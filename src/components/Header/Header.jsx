836be1566ca658c14a60bf5ed799518d4111e1c9
MccGrey <chuxzzie01@yahoo.com>
2022-10-19 05:52:39 +0100

    add menu bar that houses home, cart, foods and contact

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,26 @@ import React from "react";
 
 import { Container } from "reactstrap";
 import logo from "../../assets/images/res-logo.png";
+import { NavLink, Link } from "react-router-dom";
+
+const nav__links = [
+  {
+    display: "Home",
+    path: "/home",
+  },
+  {
+    display: "Foods",
+    path: "/foods",
+  },
+  {
+    display: "Cart",
+    path: "/cart",
+  },
+  {
+    display: "Contact",
+    path: "/contact",
+  },
+];
 
 const Header = () => {
   return (
@@ -11,6 +31,17 @@ const Header = () => {
           <div className="logo">
             <img src={logo} alt="logo" />
             <h5>Tasty Treat</h5>
+          </div>
+
+          {/* ================= menu ==================*/}
+          <div className="navigation">
+            <div className="menu">
+              {nav__links.map((item, index) => (
+                <NavLink to={item.path} key={index}>
+                  {item.display}
+                </NavLink>
+              ))}
+            </div>
           </div>
         </div>
       </Container>
