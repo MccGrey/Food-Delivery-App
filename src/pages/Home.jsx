6de3ce27ffd5571bab3671b707beb48939ffb848
MccGrey <chuxzzie01@yahoo.com>
2022-11-11 17:23:59 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Helmet from "../components/Helmet/Helmet";
 import { Container } from "reactstrap";
+import { Link } from "react-router-dom";
 
 import HERO1 from "../../src/assets/images/cognac 1.png";
 import HERO2 from "../../src/assets/images/gin 1.png";
@@ -10,6 +11,12 @@ import HERO5 from "../../src/assets/images/whiskey 1.png";
 import HERO6 from "../../src/assets/images/wine 1.png";
 
 import "../../src/components/styles/hero-section.css";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import required modules
+import { Pagination } from "swiper";
 
 const Home = () => {
   const data = [
@@ -46,8 +53,38 @@ const Home = () => {
 
   return (
     <Helmet title="Home">
-      <section className="hero-section">
-        <Container></Container>
+      <section>
+        <Container>
+          <div className="hero-container">
+            <div className="hero-desc">
+              <h3>You should never run out of drinks</h3>
+              <p>
+                We are your go to source for all the drinks you need, at all
+                times. Delivered almost immediately at your doorstep
+              </p>
+              <Link to="/home">
+                <button className="shop-now"> Shop Now</button>
+              </Link>
+            </div>
+            <Swiper
+              pagination={{
+                dynamicBullets: true,
+              }}
+              modules={[Pagination]}
+              slidesPerView={1}
+              spaceBetween={40}
+              className=" mySwiper hero-swiper"
+            >
+              {data.map(({ image }, index) => {
+                return (
+                  <SwiperSlide key={index} className="hero-image">
+                    <img src={image} alt="" />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
+        </Container>
       </section>
     </Helmet>
   );
